6667332682c927e965a460af69c5c42e774392d0
hadi <hadii.tanoomand@gmail.com>
2023-04-22 10:46:07 +0430

    Reverse String

---
+++ Reverse/reverseString.js
@@ -0,0 +1,34 @@
+// # 1
+// function reverseString(inputString) {
+//   let temp = "";
+
+//   for(let i = inputString.length - 1; i >= 0; i--) {
+//     temp += inputString[i];
+//   }
+
+//   return temp;
+// }
+
+// # 2
+function reverseString(inputString) {
+  let temp = inputString.split('');
+  temp = temp.reverse();
+  return temp.join('');
+}
+
+// # 3
+// function reverseString(inputString) {
+//   let temp = "";
+
+//   for (let char of inputString) {
+//     temp = char + temp; 
+//   }
+
+//   return temp;
+// }
+
+const temp = reverseString("hello");
+console.log("temp 3  "+temp);
+
+
+module.exports = reverseString;
